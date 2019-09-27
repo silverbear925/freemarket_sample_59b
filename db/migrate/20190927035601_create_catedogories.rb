dc04e02f03821f0d7c4081e89d021ebd31afacee
mataghu <syu.mail.syu@gmail.com>
2019-09-27 13:06:33 +0900

    編集

--- db/migrate/20190927035601_create_catedogories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCatedogories < ActiveRecord::Migration[5.2]
-  def change
-    create_table :catedogories do |t|
-
-      t.timestamps
-      t.string :name,null:false
-    end
-  end
-end
