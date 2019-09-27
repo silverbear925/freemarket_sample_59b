c3ce5c22f69fc3de7841e7ca739dd9e03b5d9401
mataghu <syu.mail.syu@gmail.com>
2019-09-27 13:00:21 +0900

    商品詳細

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_022103) do
+ActiveRecord::Schema.define(version: 2019_09_27_035601) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zipcode", null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_09_27_022103) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_adresses_on_user_id"
+  end
+
+  create_table "catedogories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
   end
 
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
