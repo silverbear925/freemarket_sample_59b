3ce668dc0cb9bba60a5b42fc5f5496e892c9314e
silverbear925 <s925maru.k.k@gmail.com>
2019-09-30 20:39:13 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   get 'battle/test'
   devise_for :users, :controllers =>{
     :registrations => 'users/registrations'
@@ -30,4 +32,12 @@ Rails.application.routes.draw do
   resources :categories
 
   resources :momo
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
