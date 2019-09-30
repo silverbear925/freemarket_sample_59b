270cc018139d316ada3e45ffc193aa057338a928
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-09-30 21:46:21 +0900

    プロフィールページのビュー

---
+++ app/assets/javascripts/mypage.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $("li.notice-todo").on('click', function(){
+    $("li.notice-todo").toggleClass("action");
+    $("ul.mypage-main__tab-content__list").toggleClass("active");
+    $
+  })
+
+  $("li.transaction").on('click', function(){
+    $("li.transaction").toggleClass("action");
+    $("ul.mypage-main__list-nowwas").toggleClass("active");
+    $
+  })
+})
