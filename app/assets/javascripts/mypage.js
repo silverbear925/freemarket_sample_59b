d80d6b2404de64654525ca6370d39e3b2e9a26e7
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-10-01 11:02:39 +0900

    マイページのビュー修正

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -11,4 +11,36 @@ $(function(){
     $("ul.mypage-main__list-nowwas").toggleClass("active");
     $
   })
+
+  $(".mypage-main__tab-content__list-item").hover(function() {
+    $(this).css("background-color", "#fcfcfc");
+    
+  }, function() {
+    $(this).css("background-color", "");
+  });
+
+  $(".mypage-main__tab-content__list-go__link").hover(function() {
+    $(this).css("background-color", "rgb(248, 248, 248)");
+    
+  }, function() {
+    $(this).css("background-color", "");
+  });
+
+  $(".notice-todo").hover(function(){  
+    var mytab = $(this).children('.tab-notice-todo')
+    mytab.css("color", "rgb(102, 101, 101)");
+  
+  }, function(){ 
+    var mytab = $(this).children('.tab-notice-todo')
+    mytab.css("color", "");
+    })
+
+    $(".transaction").hover(function(){  
+      var mytabs = $(this).children('.tab-nowwas')
+      mytabs.css("color", "rgb(102, 101, 101)");
+    
+    }, function(){ 
+      var mytabs = $(this).children('.tab-nowwas')
+      mytabs.css("color", "");
+    })
 })
