396b4d1ff5db0b6798595562212d65c34f87a032
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-01 19:25:04 +0900

    時事時事

--- app/models/bland.rb
+++
@@ -1,3 +0,0 @@
-class Bland < ApplicationRecord
-  has_many :products
-end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images
   belongs_to :area
-  belongs_to :brand, optional: true
   belongs_to :category
   belongs_to :user
   # mount_uploaders :images, ImageUploader
