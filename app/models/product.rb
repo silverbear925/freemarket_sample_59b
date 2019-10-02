dc27adfa7d23da6479ad20403707600903913513
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-02 12:46:23 +0900

    validates product user

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :text, :status,:delivery_days,:burden,:way,:price, presence: true
   has_many :images
   accepts_nested_attributes_for :images
   belongs_to :area
@@ -10,5 +11,4 @@ class Product < ApplicationRecord
   enum burden:{"送料込み": 0,"着払い": 1}
   enum delivery_days:{"1~2日で発送": 0,"2~3日で発送": 1,"4~7日で発送": 3}
   enum way:{"未定": 0,"らくらくメルカリ便": 1,"ゆうメール": 3,"レターパック": 4,"普通郵便（定型、定型外": 5,"クロネコヤマト": 6,"ゆうパック": 7,"クリックポスト": 8,"ゆうパケット": 9}
-  
 end
