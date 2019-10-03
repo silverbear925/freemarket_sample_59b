b2bbcda7ff21baa0100ac7b0a599bff58b6263b9
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-10-03 14:12:20 +0900

    プロフィール編集ビュー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to edit_user_path
+  end
+
   def log_out
   end
 
   def user_edit
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :text)
   end
 
 end
