0d20b33aac5e696481350a677d0c1627ad7b064f
kmtnk0131 <54521397+kmtnk0131@users.noreply.github.com>
2019-10-04 17:31:45 +0900

    商品詳細ページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :authenticate_user!, only: [:new, :create, :transaction_buy]
+
+
 
 
   def configure_permitted_parameters
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 ﻿class ProductsController < ApplicationController
   before_action :set_product, except: [:index,:new, :create]
+  #
   
   def index
     @categories = Category.roots.all
