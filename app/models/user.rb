b0b99a04bc218e6c9e342809d634c1ac7f6ad8da
mataghu <syu.mail.syu@gmail.com>
2019-10-06 16:04:32 +0900

    validates　追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
   has_one :adress
   # has_one :adress
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+        :recoverable, :rememberable, :validatable
+
 validates :nickname, presence: true
 validates :email, presence: true
+validates :encrypted_password,presence: true,length: { minimum: 6, maximum: 200 }
 validates :family_name, presence: true
 validates :first_name, presence: true
-validates :family_name_kana, presence: true
-validates :first_name_kana, presence: true
-validates :birth_day, presence: true 
+validates :family_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+validates :first_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+validates :birth_day, presence: true
 end
