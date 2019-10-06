65c767c1e8e86dc2f7ea879c311fb3852a5ad823
silverbear925 <s925maru.k.k@gmail.com>
2019-10-06 21:27:55 +0900

    .env github上から削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,32 +1,5 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  
 
   def facebook
     callback_for(:facebook)
@@ -48,9 +21,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @sns = info[:sns]
       session[:provider] = @sns[:provider]
       session[:uid] = @sns[:uid]
-      # session["devise.sns_id"] = sns_id
       render template: "/users/registrations/new" 
-      # redirect_to new_user_registration_path
+    
     end
   end
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,15 +23,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    #binding.pry
+    
      if params[:user][:password] == "" #sns登録なら
        params[:user][:password] = "Devise.friendly_token.first(6)" #deviseのパスワード自動生成機能を使用
        params[:user][:password_confirmation] = "Devise.friendly_token.first(6)"
        super
-       # binding.pry
+     
        sns = SnsCredential.update(user_id:  @user.id)
      else #email登録なら
-       # binding.pry
+
        super
      end
    end
