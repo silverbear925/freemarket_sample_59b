8ec89141b01638992852dbad7e2f9a9839aaeb12
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-10-06 13:47:04 +0900

    www

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_050923) do
+ActiveRecord::Schema.define(version: 2019_10_06_044318) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zipcode", null: false
