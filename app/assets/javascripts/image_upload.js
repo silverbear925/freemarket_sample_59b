d17138f43ccf0df8be677f281fbabb9c2f4b224b
kuroharu0430 <kuroharu0430@gmail.com>
2019-10-07 19:35:35 +0900

    商品登録

--- app/assets/javascripts/image_upload.js
+++ app/assets/javascripts/image_upload.js
@@ -1,24 +1,31 @@
 $(function(){
-//   $('.file_form').change(function(){
-//     console.log('dada')
-//     var file = $(this).prop('files');
-//     console.log(file)
-//     var fileReader = new FileReader();
-//         fileReader.onloadend = function() {
-//             $('.sell__image-select').html('<img src="' + fileReader.result + '"/>');
-//         }
-//         fileReader.readAsDataURL(file);
-//   })
+
+  let index = 0
     $(function(){
-      $('.file_form').change(function(){
-        console.log('DADA')
+      $('.sell__image-select').on('change','.file_form',function(){
           var file = $(this).prop('files')[0];
-          console.log(file)
           var fileReader = new FileReader();
+
           fileReader.onloadend = function() {
-              $('.sell__image-select-area').eq(0).append('<img class="output" src="' + fileReader.result + '"/>');
+            $('.sell__image-output').append(`<div class="image-wrapper"><img data_index=${index} class="output" src="${fileReader.result}"><div class="image_remove" data_index=${index}>削除</div></div>`);
+            append_html()
           }
+
           fileReader.readAsDataURL(file);
+          var append_html =function(){
+          index ++
+          $('.sell__image-select').append(`<label data_index=${index} class="label" for="product_images_attributes_${index}_src">
+                                            <input class="file_form" type="file" name="product[images_attributes][${index}][src]" id="product_images_attributes_${index}_src">
+                                           </label>`)
+          }
       });
+      $('.sell__image-output').on("click",'.image_remove',function(){
+        console.log('dada')
+        $(this).parent().remove()
+        var remove_index = $(this).attr("data_index")
+        console.log(remove_index)
+        var label =$('.label')
+        $(`label[data_index=${remove_index}]`).remove()
+      })
     });
 })
