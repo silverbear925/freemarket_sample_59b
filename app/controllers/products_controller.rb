b0efd4f9c5d26fe85994ee45bd85696c7a42aafa
silverbear925 <s925maru.k.k@gmail.com>
2019-10-07 14:50:06 +0900

    payjp_envファイルから移動

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,7 +49,7 @@
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -60,7 +60,7 @@
   def pay
     @product = Product.find(params[:id])
     card = Card.find_by(params[:id])
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
     Payjp::Charge.create(
     amount: @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     customer: card.customer_id, #顧客ID
