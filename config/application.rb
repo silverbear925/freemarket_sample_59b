b3be4787dad07924575814f1b57d63e339a254df
silverbear925 <s925maru.k.k@gmail.com>
2019-10-09 19:47:40 +0900

    ログインパスワード修正

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module FreemarketSample59b
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja #追記
+    config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
